8ab0772f3ecbd2d82593dae72d268bb0ec2abec1
Luke W <lwahlmeier@empshield.net>
2020-10-05 15:14:15 -0500

    API accessed
    Was able to access API successfully, with correct tokens. Way of handling is awful, may need to clean up for ease of use in future.

--- apicontrol.js
+++ apicontrol.js
@@ -3,33 +3,23 @@ const fs = require("fs");
 const path = require("path");
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const fetch = require("node-fetch");
+const qs = require("qs");
 
 
 const client_id = "fA8opoKWuuOAT74zaqHWs25ke6gdTZ1i";
 const client_secret = "bOIpbcG6joybFtSe";
 
-// function httpGetAsync(theUrl, callback)
-// {
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.onreadystatechange = function() { 
-//         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-//             callback(xmlHttp.responseText);
-//             console.log(XMLHttpRequest.responseText);
-//     };
-//     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-//     xmlHttp.send(null);
-// };
-
-// httpGetAsync("https://api.digikey.com/Search/v3/Products/{641-1634-2-ND}");
-
 // MAIN CALL (change info after Products)
-// https://api.digikey.com/Search/v3/Products/641-1634-2-ND?includes=ProductUrl%2CPrimaryDatasheet
+// https://api.digikey.com/Search/v3/Products/{PRODUCT_ID}?includes=ProductUrl%2CPrimaryDatasheet
 
 // FIRST STEP: API AUTHORIZATION CODE URL ENDPOINT (1 min before expires)
 // https://api.digikey.com/v1/oauth2/authorize?response_type=code&client_id=fA8opoKWuuOAT74zaqHWs25ke6gdTZ1i&redirect_uri=https%3A%2F%2Flocalhost
 
-
 // SECOND STEP: example authorization code POST request (30 min before expires)
+//
+// EXAMPLE
+//
+//
 // POST /v1/oauth2/token HTTP/1.1
 // Host: api.digikey.com
 // Content-Type: application/x-www-form-urlencoded
@@ -40,28 +30,32 @@ const client_secret = "bOIpbcG6joybFtSe";
 // redirect_uri=https://localhost
 // grant_type=authorization_code
 
-// KNOWN BUGS: format of options and authData are incorrect. Not sure if they need to match Digi-Key's API or can be any
+// const authData = {
+//     code: "H7dKOEaB",
+//     client_id: "fA8opoKWuuOAT74zaqHWs25ke6gdTZ1i",
+//     client_secret: "bOIpbcG6joybFtSe",
+//     redirect_uri: "https://localhost",
+//     grant_type: "authorization_code"
+// };
 
-const authData = {
-    "code": "2DAT7yIy",
-    "client_id": "fA8opoKWuuOAT74zaqHWs25ke6gdTZ1i",
-    "client_secret": "bOIpbcG6joybFtSe",
-    "redirect_uri": "https://localhost",
-    "grant_type": "authorization_code"
-};
+// console.log(qs.stringify(authData));
 
-fetch("https://api.digikey.com/v1/oauth2/token", {
-    method: "POST",
-    headers: {
-        "Host": "api.digikey.com",
-        "Content-Type": "application/json"
-    },
-    body: authData
-}).then((res) => res.json())
-.then((data) => console.log(data))
-.catch((err) => console.log(err))
+// fetch("https://api.digikey.com/v1/oauth2/token", {
+//     method: "POST",
+//     headers: {
+//         "Host": "api.digikey.com",
+//         "Content-Type": "application/x-www-form-urlencoded"
+//     },
+//     body: qs.stringify(authData)
+// }).then((res) => res.json())
+// .then((json) => console.log(json))
+// .catch((err) => console.log(err))
 
 //THIRD STEP (optional): getting refresh token (90 days before expires)
+//
+// EXAMPLE
+//
+//
 // POST /v1/oauth2/token HTTP/1.1
 // Host: api.digikey.com
 // Content-Type: application/x-www-form-urlencoded
@@ -70,3 +64,57 @@ fetch("https://api.digikey.com/v1/oauth2/token", {
 // client_secret={application_client_secret}&
 // refresh_token=123Asfeksodk/jkdsoieDSioIOS-483LidkOOl&
 // grant_type=refresh_token
+
+// const refreshData = {
+//     client_id: "fA8opoKWuuOAT74zaqHWs25ke6gdTZ1i",
+//     client_secret: "bOIpbcG6joybFtSe",
+//     refresh_token: "kC3kCt7zwSLsAQCVGw4l1AC2bqf8fmcL",
+//     grant_type: "refresh_token"
+// };
+
+// fetch("https://api.digikey.com/v1/oauth2/token", {
+//     method: "POST",
+//     headers: {
+//         "Host": "api.digikey.com",
+//         "Content-Type": "application/x-www-form-urlencoded"
+//     },
+//     body: qs.stringify(refreshData)
+// }).then((res) => res.json())
+// .then((json) => console.log(json))
+// .catch((err) => console.log(err))
+
+// ACCESS TOKEN: pCqU7myrSdALJWCLrrtlBoABanLA
+// REFRESH TOKEN: aRV7MJGGyuI1Vjl8YqC1040RiNcrMA7U
+
+// LAST STEP: making an API call using GET
+//
+// EXAMPLE:
+//
+// GET /Search/v3/Products/p5555-nd HTTP/1.1
+// Host: api.digikey.com
+// X-DIGIKEY-Client-Id: heWGx9w6DK8kZf3jRv5E9jUAhXrGBU67
+// Authorization: Bearer s4T5DbmFZadjNRAEbUnN9zkU3DBj
+// X-DIGIKEY-Locale-Site: US
+// X-DIGIKEY-Locale-Language: en
+// X-DIGIKEY-Locale-Currency: USD
+// X-DIGIKEY-Locale-ShipToCountry: us
+// X-DIGIKEY-Customer-Id: 0
+
+
+// main method the gets ProductURL and PrimaryDatasheet with given product_ID
+fetch("https://api.digikey.com/Search/v3/Products/641-1634-2-ND?includes=ProductUrl%2CPrimaryDatasheet", {
+    method: "GET",
+    headers: {
+        "Host": "api.digikey.com",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-DIGIKEY-Client-Id": "fA8opoKWuuOAT74zaqHWs25ke6gdTZ1i",
+        // 30 min Auth Bearer Token
+        "Authorization": "Bearer pCqU7myrSdALJWCLrrtlBoABanLA",
+        "X-DIGIKEY-Locale-Site": "US",
+        "X-DIGIKEY-Locale-Language": "en",
+        "X-DIGIKEY-Locale-Currency": "USD",
+        "X-DIGIKEY-Locale-ShipToCountry": "us"
+    }
+}).then((res) => res.json())
+.then((json) => console.log(json))
+.catch((err) => console.log(err))
