5b726056a288104526d731a061515bf0177d4629
Luke W <lwahlmeier@empshield.net>
2020-10-06 09:28:39 -0500

    Finished writeFiles method
    main portion of writeFiles method is finished.
    - may need to replace http/https npm module with something more adaptable

--- filecontrol.js
+++ filecontrol.js
@@ -2,6 +2,9 @@
 const fs = require("fs");
 const path = require("path");
 const http = require("http");
+const https = require("https");
+const express = require("express");
+const app = express();
 const createShortcutUrl = require("create-url-shortcut");
 
 // testing URL (need to replace with URL from digi-key API)
@@ -13,15 +16,15 @@ var componentShortURL = createShortcutUrl(testURL);
 // root directory file path to be saved in
 const rootFilePath = path.join(__dirname, "Component Files");
 // current file name
-var curFileName = "test.url";
+// var curFileName = "test.url";
 
-// saving the shortcut object to a url file
-fs.writeFile(path.join(rootFilePath, curFileName), componentShortURL.value, (err) => {
-    if(err) {
-        return console.log(err);
-    } 
-    console.log("The file was saved!");
-});
+// // saving the shortcut object to a url file
+// fs.writeFile(path.join(rootFilePath, curFileName), componentShortURL.value, (err) => {
+//     if(err) {
+//         return console.log(err);
+//     } 
+//     console.log("The file was saved!");
+// });
 
 
 // productID: 541-10.0KAFTR-ND
@@ -36,6 +39,9 @@ var writeFiles = (productID, productURL, productDatasheet, index) => {
     var productURL = createShortcutUrl(productURL);
     var productDatasheet = productDatasheet;
 
+    // updatedPath with index and product ID
+    var updatedPath = path.join(rootFilePath, folderName);
+
     // create new folder in rootFilePath
     // make sure updated Path is correct!!!
     fs.mkdir(updatedPath, function(err) {
@@ -45,9 +51,6 @@ var writeFiles = (productID, productURL, productDatasheet, index) => {
             console.log("New directory successfully created.");
         }
     });
-    
-    // updatedPath with index and product ID
-    var updatedPath = path.join(rootFilePath, folderName);
 
     // save the shortcut for the product as a .url file
     fs.writeFile(path.join(updatedPath, shortFileName), productURL.value, (err) => {
@@ -57,6 +60,20 @@ var writeFiles = (productID, productURL, productDatasheet, index) => {
         console.log("The shortcut was saved!");
     });
 
-    //save the pdf for the product as a .pdf file
+    // //save the pdf for the product as a .pdf file
+    // app.get('/download', (req, res) => {
+    //     // filepath and name of pdf file
+    //     const file = `${updatedPath}/${datasheetFileName}`;
+    //     res.download(file);
+    //     console.log("The pdf was saved");
+    // });
+
+    // http and https response
+    const file = fs.createWriteStream(`${updatedPath}/${datasheetFileName}`);
+    https.get(productDatasheet, function(response) {
+        response.pipe(file);
+        console.log("The pdf was saved");
+    });
 };
 
+writeFiles("541-10.0KAFTR-ND", "http://www.digikey.com/en/products/detail/vishay-dale/CRCW251210K0FKEG/1173663?s=N4IgTCBcDaIKwBYCMBaJAGAdOg0gQQDEAVAJRQDkAREAXQF8g", "http://www.comchiptech.com/admin/files/product/ACGRKM4001-HF%20Thru.%20ACGRKM4007-HF%20RevC.pdf", 2);
